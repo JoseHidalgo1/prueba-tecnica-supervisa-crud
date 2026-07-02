a34d75db2ffab9e96e56917a64f6f5a36d3551a2
Jose Hidalgo <jobohiru@gmail.com>
2026-07-02 14:15:19 -0500

    fix: initialize Spanish locale for date formatting
    Add WidgetsFlutterBinding.ensureInitialized() and await before
    initializeDateFormatting('es') to prevent LocaleDataException
    when using DateFormat.yMMMd('es').

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:provider/provider.dart';
 
 import 'package:supervisa_task_manager/providers/task_provider.dart';
 import 'package:supervisa_task_manager/screens/task_list_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('es');
   runApp(
     ChangeNotifierProvider(
       create: (_) => TaskProvider(),
